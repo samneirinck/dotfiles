5c54a50972b9bc3535733201b63a677a003774fb
Sam Neirinck <sam@samneirinck.com>
2017-02-15 08:01:12 +0100

    Make agnoster more like zsh-agnoster

--- powershell/PoshThemes/SamAgnoster.psm1
+++ powershell/PoshThemes/SamAgnoster.psm1
@@ -1,33 +1,34 @@
 #requires -Version 2 -Modules posh-git
-function Write-Theme
-{
+function Write-Theme {
     param(
         [bool]
         $lastCommandFailed,
         [string]
         $with
     )
+    $isElevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
 
     $lastColor = $sl.Colors.PromptBackgroundColor
 
     $status = Get-VCSStatus
-    Write-Prompt -Object $sl.PromptSymbols.StartSymbol -ForegroundColor $sl.Colors.SessionInfoForegroundColor -BackgroundColor $sl.Colors.PromptBackgroundColor
 
     #check the last command state and indicate if failed
-    If ($lastCommandFailed)
-    {
-        Write-Prompt -Object "$($sl.PromptSymbols.FailedCommandSymbol) " -ForegroundColor $sl.Colors.CommandFailedIconForegroundColor -BackgroundColor $sl.Colors.PromptBackgroundColor
+    If ($lastCommandFailed) {
+        Write-Prompt -Object " $($sl.PromptSymbols.FailedCommandSymbol) " -ForegroundColor $sl.Colors.CommandFailedIconForegroundColor -BackgroundColor $sl.Colors.SessionInfoBackgroundColor
     }
 
     #check for elevated prompt
-    If (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator'))
-    {
-        Write-Prompt -Object "$($sl.PromptSymbols.ElevatedSymbol) " -ForegroundColor $sl.Colors.AdminIconForegroundColor -BackgroundColor $sl.Colors.SessionInfoBackgroundColor
+    If ($isElevated) {
+        Write-Prompt -Object " $($sl.PromptSymbols.ElevatedSymbol) " -ForegroundColor $sl.Colors.AdminIconForegroundColor -BackgroundColor $sl.Colors.SessionInfoBackgroundColor
+    }
+
+    if ($isElevated -or $lastCommandFailed){
+        Write-Prompt -Object $sl.PromptSymbols.SegmentForwardSymbol -ForegroundColor $sl.Colors.SessionInfoBackgroundColor -BackgroundColor $sl.Colors.PromptHighlightColor
     }
 
     # Writes the drive portion
-    if ($status)
-    {
+    Write-Prompt -Object ' ' -ForegroundColor $sl.Colors.PromptForegroundColor -BackgroundColor $sl.Colors.PromptBackgroundColor
+    if ($status) {
         $rootPath = Split-Path -Parent $status.GitDir
         $gitRepoName = [System.IO.Path]::GetFileName($rootPath)
         Write-Prompt -Object $pwd.Path.Replace($rootPath, $gitRepoName) -ForegroundColor $sl.Colors.PromptForegroundColor -BackgroundColor $sl.Colors.PromptBackgroundColor
@@ -35,19 +36,15 @@ function Write-Theme
         Write-Prompt -Object (Get-ShortPath -dir $pwd) -ForegroundColor $sl.Colors.PromptForegroundColor -BackgroundColor $sl.Colors.PromptBackgroundColor
     }
     
-    Write-Prompt -Object ' ' -ForegroundColor $sl.Colors.PromptForegroundColor -BackgroundColor $sl.Colors.PromptBackgroundColor
+        Write-Prompt -Object $sl.PromptSymbols.SegmentForwardSymbol -ForegroundColor $sl.Colors.PromptBackgroundColor -BackgroundColor $lastColor
 
-    if ($status)
-    {
+    if ($status) {
         $themeInfo = Get-VcsInfo -status ($status)
         $lastColor = $themeInfo.BackgroundColor
-        Write-Prompt -Object $sl.PromptSymbols.SegmentForwardSymbol -ForegroundColor $sl.Colors.PromptBackgroundColor -BackgroundColor $lastColor
         Write-Prompt -Object " $($themeInfo.VcInfo) " -BackgroundColor $lastColor -ForegroundColor $sl.Colors.GitForegroundColor        
     }
 
-    if ($with)
-    {
-        Write-Prompt -Object $sl.PromptSymbols.SegmentForwardSymbol -ForegroundColor $lastColor -BackgroundColor $sl.Colors.WithBackgroundColor
+    if ($with) {
         Write-Prompt -Object " $($with.ToUpper()) " -BackgroundColor $sl.Colors.WithBackgroundColor -ForegroundColor $sl.Colors.WithForegroundColor
         $lastColor = $sl.Colors.WithBackgroundColor
     }
@@ -64,3 +61,7 @@ $sl.Colors.PromptHighlightColor = [ConsoleColor]::DarkBlue
 $sl.Colors.GitForegroundColor = [ConsoleColor]::Black
 $sl.Colors.WithForegroundColor = [ConsoleColor]::White
 $sl.Colors.WithBackgroundColor = [ConsoleColor]::DarkRed
+$sl.Colors.SessionInfoBackgroundColor = [ConsoleColor]::DarkGray
+$sl.Colors.AdminIconForegroundColor = [ConsoleColor]::DarkGreen
+
+Set-PSReadlineOption -TokenKind Command -ForegroundColor DarkGreen
