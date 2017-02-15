f567d02a49d11b0fed300485c0e5470fa3e4ae3f
Sam Neirinck <sam@samneirinck.com>
2017-02-15 11:29:33 +0100

    Add installatino of modules in bootstrap

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,5 +1,6 @@
 $profileDir = Split-Path -Parent $PROFILE
 
+"1. Setting up symbolic links"
 # Symbolic link all directories in powershell/
 Get-ChildItem -Directory powershell | ForEach-Object {
     $symPath = Join-Path $profileDir $_.BaseName 
@@ -17,3 +18,8 @@ Get-ChildItem -File conemu | ForEach-Object {
     $symPath = Join-Path %appdata% $_.Name 
     cmd /c mklink "$symPath" $_.FullName
 }
+
+""
+"2. Installing/Updating modules"
+Install-Module -Name @('posh-git', 'posh-docker', 'oh-my-posh') -Scope CurrentUser
+Update-Module
