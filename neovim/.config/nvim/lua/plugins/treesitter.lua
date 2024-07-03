06fe07358da5250cd4f2a79abc3dd88713656e18
Sam Neirinck <github@samneirinck.be>
2024-07-03 22:46:42 +0200

    Add treesitter context

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
 	-- Highlight, edit, and navigate code
 	'nvim-treesitter/nvim-treesitter',
-	build = ':TSUpdate',
 	dependencies = {
 		'nvim-treesitter/nvim-treesitter-textobjects',
 	},
