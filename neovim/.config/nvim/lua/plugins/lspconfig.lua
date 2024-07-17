ef141f304299e83b78962492724788e048233d55
Sam Neirinck <github@samneirinck.be>
2024-07-17 23:59:30 +0200

    Leader+f for formatting via LSP

--- neovim/.config/nvim/lua/plugins/lspconfig.lua
+++ neovim/.config/nvim/lua/plugins/lspconfig.lua
@@ -72,6 +72,8 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          map('<leader>f', vim.lsp.buf.format, '[F]ormat')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
