e388ebc722e51829b3a4187602576dc06542d69d
Sam Neirinck <github@samneirinck.be>
2024-07-17 23:59:07 +0200

    Add formatting commands

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -7,3 +7,8 @@ require 'config/keymaps'
 require 'lazy-bootstrap'
 require 'lazy-plugins'
 
+vim.api.nvim_create_user_command("Format", function()
+	vim.lsp.buf.format()
+end, {})
+vim.api.nvim_create_user_command("FormatJson", '%!jq .', {})
+
