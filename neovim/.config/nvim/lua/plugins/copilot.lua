5a24b78a3d1a32a390b2f5feb74f7b11e9bda2dc
Sam Neirinck <github@samneirinck.be>
2024-09-11 23:37:06 +0200

    feat(neovim): add CopilotChat integration and keybindings
    - Integrated CopilotChat with setup and autocmd for COMMIT_EDITMSG
    - Added keybindings for various CopilotChat functionalities
    - Commented out TestSuite keybinding in test.lua

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++ neovim/.config/nvim/lua/plugins/copilot.lua
@@ -10,8 +10,41 @@ return {
 			{ 'github/copilot.vim' },
 			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
 		},
-		opts = {
-			debug = false,
-		}
+		config = function(_, opts)
+			local chat = require('CopilotChat')
+			chat.setup(opts)
+
+			vim.api.nvim_create_autocmd('BufEnter', {
+				pattern = 'COMMIT_EDITMSG',
+				callback = function()
+					vim.wo.spell = true
+					vim.api.nvim_win_set_cursor(0, { 1, 0 })
+					if vim.fn.getline(1) == '' then
+						vim.cmd 'startinsert!'
+					end
+				end,
+			})
+		end,
+		keys = {
+			{
+				'<leader>aq',
+				function()
+					local input = vim.fn.input('Quick Chat: ')
+					if input ~= '' then
+						require('CopilotChat').ask(input,
+							{ selection = require('CopilotChat.select').buffer })
+					end
+				end,
+				desc = 'Quick Chat',
+			},
+			{ '<leader>ac', '<cmd>CopilotChatToggle<cr>',   desc = 'Toggle Chat' },
+			{ '<leader>ae', '<cmd>CopilotChatExplain<cr>',  mode = { 'n', 'v' }, desc = 'Explain' },
+			{ '<leader>ar', '<cmd>CopilotChatReview<cr>',   mode = { 'n', 'v' }, desc = 'Review' },
+			{ '<leader>af', '<cmd>CopilotChatFix<cr>',      mode = { 'n', 'v' }, desc = 'Fix' },
+			{ '<leader>ao', '<cmd>CopilotChatOptimize<cr>', mode = { 'n', 'v' }, desc = 'Optimize' },
+			{ '<leader>ad', '<cmd>CopilotChatDocs<cr>',     mode = { 'n', 'v' }, desc = 'Docs' },
+			{ '<leader>at', '<cmd>CopilotChatTests<cr>',    mode = { 'n', 'v' }, desc = 'Tests' },
+			{ '<leader>am', '<cmd>CopilotChatCommitStaged<cr>',   mode = { 'n', 'v' }, desc = 'Commit' },
+		},
 	}
 }
--- neovim/.config/nvim/lua/plugins/test.lua
+++ neovim/.config/nvim/lua/plugins/test.lua
@@ -4,7 +4,7 @@ return {
 		config = function()
 				vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", {})
 				vim.keymap.set("n", "<leader>T", ":TestFile<CR>", {})
-				vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", {})
+				-- vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", {})
 				vim.keymap.set("n", "<leader>l", ":TestLast<CR>", {})
 				vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", {})
 				vim.g['test#strategy'] = 'neovim'
