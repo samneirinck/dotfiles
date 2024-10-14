6ad9a1e8346c89d52b6dcbf2ca494f51d9fd981e
Sam Neirinck <github@samneirinck.be>
2024-10-14 23:07:25 +0200

    Figure out smart splits

--- neovim/.config/nvim/lua/plugins/smart-splits.lua
+++ neovim/.config/nvim/lua/plugins/smart-splits.lua
@@ -1,8 +1,12 @@
 return {
   {
     'mrjones2014/smart-splits.nvim',
-    opts = {
-
+    lazy = false,
+    keys = {
+      { '<C-j>', '<cmd>SmartCursorMoveDown<CR>' },
+      { '<C-k>', '<cmd>SmartCursorMoveUp<CR>' },
+      { '<C-h>', '<cmd>SmartCursorMoveLeft<CR>' },
+      { '<C-l>', '<cmd>SmartCursorMoveRight<CR>' },
     },
   }
 }
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -20,17 +20,8 @@ config.window_padding = {
   bottom = 0
 }
 
-
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.scrollback_lines = 10000
-
-local function move_pane(key, direction)
-  return {
-    key = key,
-    mods = "LEADER",
-    action = wezterm.action.ActivatePaneDirection(direction)
-  }
-end
 
 local function resize_pane(key, direction)
   return {
@@ -39,6 +30,58 @@ local function resize_pane(key, direction)
   }
 end
 
+-- Use the defaults as a base
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- make task numbers clickable
+table.insert(config.hyperlink_rules, {
+  regex = [[\b(SP-\d+)\b]],
+  format = 'https://showpad.atlassian.net/browse/$1',
+})
+
+-- make username/project paths clickable. this implies paths like the following are for github.
+-- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
+-- as long as a full url hyperlink regex exists above this it should not match a full url to
+-- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
+table.insert(config.hyperlink_rules, {
+  regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+  format = 'https://www.github.com/$1/$3',
+})
+
+
+-- smart splits
+local function is_vim(pane)
+  -- this is set by the plugin, and unset on ExitPre in Neovim
+  return pane:get_user_vars().IS_NVIM == 'true'
+end
+
+local direction_keys = {
+  h = 'Left',
+  j = 'Down',
+  k = 'Up',
+  l = 'Right',
+}
+
+local function split_nav(resize_or_move, key)
+  return {
+    key = key,
+    mods = resize_or_move == 'resize' and 'META' or 'CTRL',
+    action = wezterm.action_callback(function(win, pane)
+      if is_vim(pane) then
+        -- pass the keys through to vim/nvim
+        win:perform_action({
+          SendKey = { key = key, mods = resize_or_move == 'resize' and 'META' or 'CTRL' },
+        }, pane)
+      else
+        if resize_or_move == 'resize' then
+          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+        else
+          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+        end
+      end
+    end),
+  }
+end
 
 config.keys = {
   {
@@ -57,17 +100,26 @@ config.keys = {
     action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
   },
   {
+    key = 'm',
+    mods = 'LEADER',
+    action = wezterm.action.TogglePaneZoomState
+  },
+  {
     key = "s",
     mods = "LEADER",
     action = wezterm.action.ShowLauncherArgs { flags = "FUZZY|WORKSPACES", title = "Workspaces" }
   },
 
-  -- Pane navigation
-  move_pane('j', 'Down'),
-  move_pane('k', 'Up'),
-  move_pane('h', 'Left'),
-  move_pane('l', 'Right'),
-
+    -- move between split panes
+    split_nav('move', 'h'),
+    split_nav('move', 'j'),
+    split_nav('move', 'k'),
+    split_nav('move', 'l'),
+    -- resize panes
+    split_nav('resize', 'h'),
+    split_nav('resize', 'j'),
+    split_nav('resize', 'k'),
+    split_nav('resize', 'l'),
     {
     -- When we push LEADER + R...
     key = 'r',
@@ -92,25 +144,6 @@ config.key_tables = {
     resize_pane('l', 'Right'),
   },
 }
-
-
--- Use the defaults as a base
-config.hyperlink_rules = wezterm.default_hyperlink_rules()
-
--- make task numbers clickable
-table.insert(config.hyperlink_rules, {
-  regex = [[\b(SP-\d+)\b]],
-  format = 'https://showpad.atlassian.net/browse/$1',
-})
-
--- make username/project paths clickable. this implies paths like the following are for github.
--- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
--- as long as a full url hyperlink regex exists above this it should not match a full url to
--- github or gitlab / bitbucket (i.e. https://gitlab.com/user/project.git is still a whole clickable url)
-table.insert(config.hyperlink_rules, {
-  regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-  format = 'https://www.github.com/$1/$3',
-})
 
 return config
 
