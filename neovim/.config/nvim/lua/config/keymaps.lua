46bb96afac2d84b499ea785730b863cfe91a0d4c
Sam Neirinck <github@samneirinck.be>
2024-12-10 23:28:31 +0100

    Format lua on save

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -10,14 +10,3 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
--- neovim/.config/nvim/lua/plugins/test.lua
+++ neovim/.config/nvim/lua/plugins/test.lua
@@ -2,14 +2,15 @@ return {
 	{
 		'vim-test/vim-test',
 		config = function()
-				vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", {})
-				vim.keymap.set("n", "<leader>T", ":TestFile<CR>", {})
-				-- vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", {})
-				vim.keymap.set("n", "<leader>l", ":TestLast<CR>", {})
-				vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", {})
-				vim.g['test#strategy'] = 'neovim'
+			vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", {})
+			vim.keymap.set("n", "<leader>T", ":TestFile<CR>", {})
+			-- vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", {})
+			vim.keymap.set("n", "<leader>l", ":TestLast<CR>", {})
+			vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", {})
+			vim.g['test#strategy'] = 'neovim'
 
-				vim.g['test#php#phpunit#executable'] = 'docker --log-level ERROR compose -f ../docker-compose.yml -f docker-compose.yml exec centralstation bin/phpunit --config app/phpunit.xml.dist'
+			vim.g['test#php#phpunit#executable'] =
+			'docker --log-level ERROR compose -f ../docker-compose.yml -f docker-compose.yml exec centralstation bin/phpunit --config app/phpunit.xml.dist'
 		end,
 	},
 }
