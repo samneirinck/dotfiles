760e114b7be7bcfd63520668939686bb25993489
Sam Neirinck <github@samneirinck.be>
2025-01-06 09:03:43 +0100

    Make codecompanion a bit nicer

--- neovim/.config/nvim/lua/plugins/codecompanion.lua
+++ neovim/.config/nvim/lua/plugins/codecompanion.lua
@@ -5,17 +5,29 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
     },
-    config = function()
-      require("codecompanion").setup({
-        strategies = {
-          chat = {
-            adapter = "copilot",
-          },
-          inline = {
-            adapter = "copilot",
-          }
+    opts = {
+      adapters = {
+        copilot = function()
+          return require("codecompanion.adapters").extend("copilot", {
+            schema = {
+              model = {
+                default = "claude-3.5-sonnet"
+              }
+            }
+          })
+        end,
+      },
+      strategies = {
+        chat = {
+          adapter = "copilot",
+        },
+        inline = {
+          adapter = "copilot",
         }
-      })
-    end,
+      }
+    },
+    keys = {
+      { "<leader>ac", "<cmd>CodeCompanionChat<CR>", desc = "Toggle Code Companion Chat" }
+    }
   }
 }
