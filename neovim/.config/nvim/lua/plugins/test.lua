5fc4482a102c059f8866fa0342eb12990fbf6d4f
Sam Neirinck <github@samneirinck.be>
2025-01-07 09:26:17 +0100

    Change test invocation to <leader>t

--- neovim/.config/nvim/lua/plugins/test.lua
+++ neovim/.config/nvim/lua/plugins/test.lua
@@ -2,11 +2,11 @@ return {
 	{
 		'vim-test/vim-test',
 		config = function()
-			vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", {})
-			vim.keymap.set("n", "<leader>T", ":TestFile<CR>", {})
+			vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>", {})
+			vim.keymap.set("n", "<leader>tT", ":TestFile<CR>", {})
 			-- vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", {})
-			vim.keymap.set("n", "<leader>l", ":TestLast<CR>", {})
-			vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", {})
+			vim.keymap.set("n", "<leader>tl", ":TestLast<CR>", {})
+			vim.keymap.set("n", "<leader>tg", ":TestVisit<CR>", {})
 			vim.g['test#strategy'] = 'neovim'
 
 			vim.g['test#php#phpunit#executable'] =
