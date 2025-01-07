c719c26409440002b4d8e510982e5cab8cee4fe5
Sam Neirinck <github@samneirinck.be>
2025-01-07 09:26:40 +0100

    Add git key maps

--- neovim/.config/nvim/lua/plugins/fugitive.lua
+++ neovim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,4 +1,10 @@
 return {
-	{ 'tpope/vim-fugitive' },
-	{ 'shumphrey/fugitive-gitlab.vim' }
+  {
+    'tpope/vim-fugitive',
+    keys = {
+      { "<leader>gs", "<cmd>0Git<CR>",      desc = "Git Status" },
+      { "<leader>gb", "<cmd>Git blame<CR>", desc = "Git Blame" },
+    }
+  },
+  { 'shumphrey/fugitive-gitlab.vim' }
 }
