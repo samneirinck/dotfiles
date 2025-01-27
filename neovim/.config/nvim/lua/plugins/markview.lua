fec073f1f895a4d5cc8b17612714f39d91d3360e
Sam Neirinck <github@samneirinck.be>
2025-01-27 21:20:17 +0100

    Add markview

---
+++ neovim/.config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "OXY2DEV/markview.nvim",
+    ft = { "markdown", "codecompanion" },
+    lazy = false,
+    opts = {
+      preview = {
+        filetypes = { "markdown", "codecompanion" },
+        ignore_buftypes = {}
+      },
+    },
+  }
+}
