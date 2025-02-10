aa47621443602ead1a92ff7b3fc1e7234d8859ec
Sam Neirinck <github@samneirinck.be>
2025-02-10 21:30:55 +0100

    Specify oil config type

--- neovim/.config/nvim/lua/plugins/oil.lua
+++ neovim/.config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,12 @@
 return {
   {
     "stevearc/oil.nvim",
+    ---@module "oil"
+    ---@type oil.Config
     opts = {
+      columns = {
+        "icon",
+      },
       view_options = {
         show_hidden = true,
       },
