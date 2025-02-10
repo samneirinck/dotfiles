7e99b6ddffff7e9da0e53a06189c276d64c6f0b9
Sam Neirinck <github@samneirinck.be>
2025-02-10 20:22:32 +0100

    Coalesce support if needed

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -13,3 +13,23 @@ vim.api.nvim_create_user_command("Format", function()
 end, {})
 vim.api.nvim_create_user_command("FormatJson", '%!jq .', {})
 
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "kotlin",
+	callback = function(args)
+		local coalesce_cmd =
+		'/Users/sam.neirinck/Code/github.com/samneirinck/coalesce/build/install/coalesce/bin/coalesce'
+		if vim.fn.executable(coalesce_cmd) == 1 then
+			vim.lsp.start({
+				name = "coalesce",
+				cmd = { coalesce_cmd },
+				cmd_env = {
+					JAVA_OPTS =
+					'-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005'
+				},
+				root_dir = vim.fs.root(args.buf, { 'build.gradle', 'build.gradle.kts' })
+			})
+		else
+			vim.notify("Coalesce LSP command not found: " .. coalesce_cmd, vim.log.levels.WARN)
+		end
+	end
+})
