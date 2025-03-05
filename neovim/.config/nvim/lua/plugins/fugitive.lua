5817cfac7b6a11c7ee858b42297c03cb53423a37
Sam Neirinck <github@samneirinck.be>
2025-03-05 23:26:03 +0100

    Don't lazy load fugitive

--- neovim/.config/nvim/lua/plugins/fugitive.lua
+++ neovim/.config/nvim/lua/plugins/fugitive.lua
@@ -1,6 +1,7 @@
 return {
   {
     'tpope/vim-fugitive',
+    lazy = false,
     keys = {
       { "<leader>gs", "<cmd>0Git<CR>",      desc = "Git Status" },
       { "<leader>gb", "<cmd>Git blame<CR>", desc = "Git Blame" },
