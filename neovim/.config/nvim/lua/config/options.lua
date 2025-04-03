9345770f048b2a44369aef7f558071e6b68d8b09
Sam Neirinck <github@samneirinck.be>
2025-04-03 09:22:41 +0200

    Add some neovim settings

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -1,53 +1,56 @@
 -- Set highlight on search
-vim.o.hlsearch = true
+vim.o.hlsearch        = true
 
 -- Make line numbers default
-vim.wo.number = true
+vim.wo.number         = true
 vim.wo.relativenumber = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse           = 'a'
 
-vim.opt.showmode = false
+vim.opt.showmode      = false
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard       = 'unnamedplus'
 
 -- Enable break indent
-vim.o.breakindent = true
+vim.o.breakindent     = true
 
 -- Save undo history
-vim.o.undofile = true
+vim.o.undofile        = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.o.ignorecase      = true
+vim.o.smartcase       = true
 
 -- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
+vim.wo.signcolumn     = 'yes'
 
 -- Decrease update time
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+vim.o.updatetime      = 250
+vim.o.timeoutlen      = 300
 
 -- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.opt.splitright    = true
+vim.opt.splitbelow    = true
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt     = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
+vim.o.termguicolors   = true
 
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.opt.inccommand    = 'split'
 
-vim.opt.cursorline = true
+vim.opt.cursorline    = true
 
-vim.shiftwidth = 2
+vim.shiftwidth        = 2
+
+-- Allow project specific settings
+vim.opt.exrc          = true
 
 -- Nicer diff
 vim.opt.fillchars:append { diff = ' ' }
