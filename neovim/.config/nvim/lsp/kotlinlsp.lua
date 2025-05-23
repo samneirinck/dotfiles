c1afa5629104222e4ccad0ea3aacd84b00660243
Sam Neirinck <github@samneirinck.be>
2025-05-23 09:13:39 +0200

    Add kotlinlsp

--- neovim/.config/nvim/lsp/kotlinlsp.lua
+++ neovim/.config/nvim/lsp/kotlinlsp.lua
@@ -1,8 +1,6 @@
 return {
-	cmd = { '/Users/sam.neirinck/Code/github.com/amgdev9/kotlin-lsp/app/build/install/app/bin/app' },
-	cmd_env = {
-		-- JAVA_OPTS = '-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:9009'
-	},
-	root_markers = { 'build.gradle', 'build.gradle.kts' },
+	cmd = { 'kotlin-ls', '--stdio' },
+	single_file_support = true,
+	root_markers = { 'build.gradle', 'build.gradle.kts', 'pom.xml' },
 	filetypes = { 'kotlin' },
 }
