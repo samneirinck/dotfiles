34a31b7a689f3d34f77030b58f4942b9848cfc30
Sam Neirinck <github@samneirinck.be>
2025-11-24 10:39:36 +0100

    Format with eslint

--- neovim/.config/nvim/lua/plugins/conform.lua
+++ neovim/.config/nvim/lua/plugins/conform.lua
@@ -2,8 +2,11 @@ return {
   {
     'stevearc/conform.nvim',
     opts = {
+      formatters_by_ft = {
+        typescript = { "prettier", "eslint_d" }
+      },
       format_on_save = {
-        timeout_ms = 500,
+        timeout_ms = 1000,
         lsp_format = "fallback",
       },
     },
